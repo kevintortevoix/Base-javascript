6ecd5ba9ce07f9a57ab980e9d6acc94a239a67e7
Kévin TORTEVOIX <golfgti72@hotmail.fr>
2025-12-05 09:30:11 +0100

    Début éxercice sur les events

--- DOM 2/Exercice 11/script.js
+++ DOM 2/Exercice 11/script.js
@@ -1,1 +1,17 @@
-let  ages = [5, 18, 25, 15, 30];
+let ages = [5, 18, 25, 15, 30];
+
+const ul = document.getElementById("listeAges");
+
+for (let i = 0; i < ages.length; i++) {
+    if (ages[i] < 18) {
+        const liste1 = document.createElement("li");
+        liste1.textContent = `mineur , ${ages[i]}`;
+        ul.appendChild(liste1)
+    }
+    else {
+        const liste2 = document.createElement("li");
+        liste2.textContent = `majeur , ${ages[i]}`;
+        ul.appendChild(liste2)
+    }
+}
+
---
+++ DOM 2/Exercice 12/script.js
@@ -0,0 +1,6 @@
+let livres = [
+    { titre: "Livre A", categorie: "Roman" },
+    { titre: "Livre B", categorie: "Poésie" },
+    { titre: "Livre C", categorie: "Roman" }
+];
+
