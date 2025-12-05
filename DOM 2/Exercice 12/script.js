daa2614e29bd80679923f381b2a6187ab310e3ad
Kévin TORTEVOIX <golfgti72@hotmail.fr>
2025-12-05 09:35:45 +0100

    Début exercice 2

--- DOM 2/Exercice 12/script.js
+++ DOM 2/Exercice 12/script.js
@@ -4,3 +4,33 @@ let livres = [
     { titre: "Livre C", categorie: "Roman" }
 ];
 
+
+const categories = {};
+livres.forEach(livre => {
+    if (!categories[livre.categorie]) {
+        categories[livre.categorie] = [];
+    }
+    categories[livre.categorie].push(livre.titre);
+});
+
+
+const container = document.createElement("div");
+
+for (let categorie in categories) {
+    const h2 = document.createElement("h2");
+    h2.textContent = categorie;
+    container.appendChild(h2);
+
+    const ul = document.createElement("ul");
+
+    categories[categorie].forEach(titre => {
+        const li = document.createElement("li");
+        li.textContent = titre;
+        ul.appendChild(li);
+    });
+
+    container.appendChild(ul);
+}
+
+document.body.appendChild(container);
+
