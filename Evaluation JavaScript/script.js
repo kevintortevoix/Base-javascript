44227a6fbc30ed2e4f815b94b0c8d55a2b36a021
Kévin TORTEVOIX <golfgti72@hotmail.fr>
2025-12-15 10:41:59 +0100

    Fin de la Partie A - Affichage des Produits (DOM)

--- Evaluation JavaScript/script.js
+++ Evaluation JavaScript/script.js
@@ -1,6 +1,53 @@
 const produits = [
-    { id: 1, nom: "Thé Vert Bio", prix: 12.99, image: "https://via.placeholder.com/150" },
-    { id: 2, nom: "Café Arabica", prix: 8.50, image: "https://via.placeholder.com/150" },
-    { id: 3, nom: "Infusion Menthe", prix: 5.00, image: "https://via.placeholder.com/150" },
-    { id: 4, nom: "Chocolat Chaud", prix: 15.00, image: "https://via.placeholder.com/150" }
+    { id: 1, nom: "Thé Vert Bio", prix: 12.99, image: "https://placehold.co/150?text=Thé+Vert+Bio" },
+    { id: 2, nom: "Café Arabica", prix: 8.50, image: "https://placehold.co/150?text=Café+Arabica" },
+    { id: 3, nom: "Infusion Menthe", prix: 5.00, image: "https://placehold.co/150?text=Infusion+Menthe" },
+    { id: 4, nom: "Chocolat Chaud", prix: 15.00, image: "https://placehold.co/150?text=Chocolat+Chaud" }
 ];
+
+const containerProduits = document.getElementById("produits-container")
+
+// Fonction pour afficher les élements dans le DOM.
+
+function afficherProduits(produits) {
+
+    containerProduits.innerHTML = "";
+
+    produits.forEach(produit => {
+        // créer le container card
+        const carte = document.createElement("div");
+        carte.classList.add('product-card');
+
+        // créer l'image 
+        const img = document.createElement("img");
+        img.src = produit.image;
+        img.alt = produit.nom; 
+
+        // créer le contenu textuel 
+
+        const divContent = document.createElement("div");
+        divContent.classList.add("card-content");
+
+        const h3 = document.createElement("h3");
+        h3.textContent = produit.nom;
+
+
+        const pPrix = document.createElement("p");
+        pPrix.classList.add("card-price");
+        pPrix.textContent = produit.prix + "€";
+
+        // assemblage :
+
+        divContent.appendChild(h3);
+        divContent.appendChild(pPrix);
+
+        carte.appendChild(img);
+        carte.appendChild(divContent);
+
+        containerProduits.appendChild(carte);
+
+    });
+
+    }
+
+    afficherProduits(produits);
